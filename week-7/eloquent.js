7605f14e85fa20bbad1ac4d9dc868299f2885403
Katherine Broner <katherine.broner@gmail.com>
2016-03-04 12:41:47 -0800

    Complete eloquent exercise

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -5,18 +5,37 @@
 // Program Structure
 // Write your own variable and do something to it.
 
+var number = 5;
+var doubleNumber = number * 2;
+console.log(doubleNumber);
 
+//var favFood = prompt("What is your favorite food?");
+//alert ("Hey! That's my favorite too!");
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
-
+var triangle = ""
+for (var num = 0; num < 7; num ++)
+  console.log(triangle += "#");
 
 
 // Functions
 
 // Complete the `minimum` exercise.
 
+function min(num1, num2) {
+  console.log(Math.min(num1, num2));
+}
 
+min(10, 5)
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {};
+me["name"] = "Katherine";
+me["age"] = 23;
+me["favoriteFoods"] = ["cheese", "chocolate", "berries"];
+me["quirk"] = "I love adult coloring books!";
+
+console.log(me);
